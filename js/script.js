0e87724b4a365396132b2937320625f2b4f0ec44
maciekmacku <maciejszalaj@gmail.com>
2018-04-10 22:18:14 +0200

    task 10.2 started

--- js/script.js
+++ js/script.js
@@ -2,22 +2,3 @@ $(function(){
 	console.log('DOM loaded - you can have fun');
 });
 
-$(function(){
-  var paragraphs = $('p').css('color', 'red');
-  var rawFirstParagraph = paragraphs[0];
-  console.log(rawFirstParagraph);
-  var jqFirstParagraph = paragraphs.eq(0)
-  console.log(jqFirstParagraph);
-  jqFirstParagraph.css('color', 'blue');
-});
-
-$(function(){
-  $('p').each(function( index, elem ) {
-    $( elem ).text('paragraf ' + index);
-  }) .css('color', 'red');
-});
-
-$(function(){
-  var li = $('nav').find('li').last();
-  li.css("color", "red");
-});
